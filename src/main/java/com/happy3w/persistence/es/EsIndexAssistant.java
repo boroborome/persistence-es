29fda43a7140fd255af4754f3bd434cf4aaeda75
Yushan Gao <ysgao@thoughtworks.com>
2020-11-14 11:45:21 +0800

    refactor: remove unused EsIndexAssistant

--- src/main/java/com/happy3w/persistence/es/EsIndexAssistant.java
+++
@@ -1,8 +0,0 @@
-package com.happy3w.persistence.es;
-
-public class EsIndexAssistant implements IEsIndexAssistant {
-
-    public String getIndexName(Class docType) {
-        return null;
-    }
-}
